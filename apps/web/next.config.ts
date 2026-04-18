1674d519bbe4f05c064b6bac17cafb820078edf1
Luiz Vergennes <luizvergennes@lgvm.dev>
2026-04-17 21:57:49 -0300

    config: bun + biome + husky

---
+++ apps/web/next.config.ts
@@ -0,0 +1,10 @@
+import "@mock-better-t/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	typedRoutes: true,
+	reactCompiler: true,
+	transpilePackages: ["@workspace/ui"],
+};
+
+export default nextConfig;
